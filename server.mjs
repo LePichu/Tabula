3f020880fbaf1ea715dbea10c0a2c78d09b4d3b0
Ishat Gupta <62379502+LePichu@users.noreply.github.com>
2026-04-24 13:30:31 +0530

    feat: deploy app on deno 2/n

--- server.mjs
+++ server.mjs
@@ -1,19 +1,18 @@
-import { Hono } from 'hono'
-import { serve } from '@hono/node-server'
-import { serveStatic } from '@hono/node-server/serve-static'
-import { renderPage } from 'vike/server'
+import { Hono } from "hono"
+import { serveStatic } from "hono/deno"
+import { renderPage } from "vike/server"
 
 const app = new Hono()
 
-app.use('/assets/*', serveStatic({ root: './dist/client' }))
+app.use("/assets/*", serveStatic({ root: "./dist/client" }))
 
-app.get('*', async (c) => {
-  const pageContext = await renderPage({ urlOriginal: c.req.url })
-  const { httpResponse } = pageContext
-  if (!httpResponse) return c.notFound()
-  const { body, statusCode, headers } = httpResponse
-  headers.forEach(([name, value]) => c.header(name, value))
-  return c.body(body, statusCode)
+app.get("*", async (c) => {
+	const pageContext = await renderPage({ urlOriginal: c.req.url })
+	const { httpResponse } = pageContext
+	if (!httpResponse) return c.notFound()
+	const { body, statusCode, headers } = httpResponse
+	headers.forEach(([name, value]) => c.header(name, value))
+	return c.body(body, statusCode)
 })
 
-serve({ fetch: app.fetch, port: Number(process.env.PORT) || 3000 })
+Deno.serve(app.fetch)
